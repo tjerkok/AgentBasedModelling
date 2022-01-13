f55faa6591a7144c5074b4e367e6256ed052ca85
Jacco <jaccov.wijk@yahoo.com>
2022-01-13 17:07:46 +0100

    Update agent.py

--- mesamodel/agent.py
+++ mesamodel/agent.py
@@ -1,4 +1,38 @@
 from mesa import Agent
 
-class OurAgent(Agent):
-    raise NotImplementedError
+class Person(Agent):
+    def __init__(self, unique_id, pos, model, objectives, moore=False, speed=1):
+        super().__init__(unique_id, model)
+        
+        self.pos = pos
+        self.objectives = objectives
+        self.current_objective = self.objectives.pop()
+        #?
+        self.model = model
+        self.speed = speed
+        self.moore = moore
+    
+    def step(self):
+        # TODO: True is voor eigen positie meenemen, willen we dat?
+        # TODO: Check if move is valid
+        possibel_moves = self.model.grid.get_neighborhood(self.pos, self.moore, True, self.speed) 
+        
+        # TODO: Choose move
+        next_move = self.random.choice(possibel_moves)
+        
+        self.model.grid.move_agent(self, next_move)
+        
+        # TODO: If current is objective, get next objective
+        # self.current_objective = self.objective.pop()
+        
+    def find_route(self, objective):
+        
+        raise NotImplementedError
+        
+
+class Obstacle(Agent):
+    def __init__(self, next_id, model, pos, obstacle_type):
+        super().__init__(next_id, model)
+        self.obstacle_type = obstacle_type
+        self.pos = pos
+        
