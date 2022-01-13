fbeabbd407702a77fee37fc1f29c61136d7cf20d
Jacco <jaccov.wijk@yahoo.com>
2022-01-13 18:28:54 +0100

    Updated agent.py

--- mesamodel/agent.py
+++ mesamodel/agent.py
@@ -1,4 +1,5 @@
 from mesa import Agent
+import numpy as np
 
 class Person(Agent):
     def __init__(self, unique_id, pos, model, objectives, moore=False, speed=1):
@@ -7,28 +8,35 @@ class Person(Agent):
         self.pos = pos
         self.objectives = objectives
         self.current_objective = self.objectives.pop()
-        #?
         self.model = model
-        self.speed = speed
-        self.moore = moore
+        # Use?
+        # self.speed = speed
+        # self.moore = moore
     
     def step(self):
-        # TODO: True is voor eigen positie meenemen, willen we dat?
-        # TODO: Check if move is valid
-        possibel_moves = self.model.grid.get_neighborhood(self.pos, self.moore, True, self.speed) 
+        # Find next step
+        next_move = self.find_route()
         
-        # TODO: Choose move
-        next_move = self.random.choice(possibel_moves)
-        
+        # Make move
         self.model.grid.move_agent(self, next_move)
         
-        # TODO: If current is objective, get next objective
-        # self.current_objective = self.objective.pop()
+    def find_route(self):
+        # TODO: True is voor eigen positie meenemen, willen we dat?
+        possibel_moves = self.model.grid.get_neighborhood(self.pos, self.moore, True, self.speed)
         
-    def find_route(self, objective):
+        # Check if at objective
+        if self.current_objective in possibel_moves:
+            self.current_objective = self.objectives.pop()
         
-        raise NotImplementedError
-        
+        # TODO: Better algorithm
+        # Check if he gets closer and not in obstacles, otherwise go up
+        obstacles = [x.pos for x in self.model.obstacles]
+        for move in possibel_moves:
+            if np.abs(move[0] - self.current_objective[0]) < np.abs(self.pos[0] - self.current_objective[0]) and move not in obstacles:
+                return move
+        x, y = self.pos
+        return (x + 1, y)
+    
 
 class Obstacle(Agent):
     def __init__(self, next_id, model, pos, obstacle_type):
