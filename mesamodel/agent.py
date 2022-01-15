b070daf2594e5bb101017f79c25f9a1a02b0e0c4
Lars1998 <90910928+Lars1998@users.noreply.github.com>
2022-01-15 11:52:54 +0100

    Update collissions and sorting groceries

--- mesamodel/agent.py
+++ mesamodel/agent.py
@@ -9,7 +9,11 @@ class Person(Agent):
         
         self.pos = pos
         self.objectives = objectives
-        print(f"Person {self.unique_id} has objs {self.objectives}")
+        print(f"Originally Person {self.unique_id} has objs {self.objectives}")
+        self.objectives_inc_coord = []
+        self.get_objectives_coord()
+        self.sort_objectives()
+        print(f"After sorting person {self.unique_id} has objs {self.new_objectives}")
         self.next_objective()
         self.model = model
         self.speed = speed
@@ -17,9 +21,30 @@ class Person(Agent):
         self.basket = []
         self.route = []
 
+
+    def get_objectives_coord(self):
+        for next_objective in self.objectives:
+            self.objectives_inc_coord.append((next_objective, random.choice(self.model.objectives[next_objective])))
+
+
+    def sort_objectives(self):
+        self.temp_object = []
+        for obj in self.objectives_inc_coord[:-1]:  # get all objects except for exit
+            self.temp_object.append((obj[0], obj[1], len(nx.astar_path(self.model.graph, self.pos, obj[1], heuristic=self.dist))-1))
+
+        x = lambda index: index[2]
+        self.temp_object.sort(key=x)
+        self.new_objectives = []
+        for obj in self.temp_object:
+            self.new_objectives.append((obj[0], obj[1]))
+        self.new_objectives.append(('exit', self.model.objectives['exit'][0]))  # [0] because of [(1,9)], list being returned
+
+
+
     def next_objective(self):
-        next_objective = self.objectives.pop(0)
-        self.current_objective = (next_objective, random.choice(self.model.objectives[next_objective]))
+        #next_objective = self.objectives.pop(0)
+        #self.current_objective = (next_objective, random.choice(self.model.objectives[next_objective]))
+        self.current_objective = self.new_objectives.pop(0)  # when sorting list
 
     def step(self):
         # Find next step
@@ -27,8 +52,15 @@ class Person(Agent):
         next_move = self.find_route()
         print(f"planned move: {next_move}")
         while self.model.grid.out_of_bounds(next_move) or not self.model.grid.is_cell_empty(next_move):
-            print("illegal move, try again")
-            next_move = self.find_route()
+            print("illegal move")
+            #next_move = self.find_route()
+            if random.random() < 0.5:
+                next_move = self.pos
+                print("illegal move planned, wait one time step")
+                break
+            else:
+                next_move = self.random_move()
+                print("try random step")
         # Make move
         self.model.grid.move_agent(self, next_move)
         if self.pos == self.current_objective[1]:
@@ -64,7 +96,8 @@ class Person(Agent):
         #         return move
 
         # Uncomment according to chosen move
-        move = self.random.choice([self.astar_move(),self.astar_move(),self.random_move()])
+        move = self.astar_move()
+        #move = self.random.choice([self.astar_move(),self.astar_move(),self.random_move()])
         #move = self.random_move()
         return move
 
