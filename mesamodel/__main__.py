d44bccfa60cc2587ba6bf1d7b3c0d23e9cb2744b
Tjerko Kieft <64585807+tjerkok@users.noreply.github.com>
2022-01-20 14:35:37 +0100

    server werkt (alleen personen)

--- mesamodel/__main__.py
+++ mesamodel/__main__.py
@@ -1,11 +1,11 @@
-from .server import server
 import os
 from server import GroceryServer
 from utils import read_json
 
 PARENT_PATH = os.getcwd()+"/mesamodel/"
 
-config = read_json(PARENT_PATH+"config1.json")
+# config = read_json(PARENT_PATH+"config1.json")
+config = read_json("config1.json")
 server = GroceryServer(config)
 server.launch()
 
